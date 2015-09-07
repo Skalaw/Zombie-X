1b29a6272e9ba034520d6fb185a4f14800f1b4a9
Skalaw <mskalski6@wp.pl>
2015-09-07 20:17:49 +0200

    [ADD] Init project

---
+++ android/src/com/asda/zombiex/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.asda.zombiex.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.asda.zombiex.ZombieGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new ZombieGame(), config);
+	}
+}
---
+++ desktop/src/com/asda/zombiex/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.asda.zombiex.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.asda.zombiex.ZombieGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new ZombieGame(), config);
+	}
+}
