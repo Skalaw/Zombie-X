bce7a064a2ebb199e8bff3e973aa6c22ef1af4ae
Skalaw <mskalski6@wp.pl>
2015-10-29 21:00:33 +0100

    [FIX] Hide navigation button in Nexus 4

--- android/src/com/asda/zombiex/android/AndroidLauncher.java
+++ android/src/com/asda/zombiex/android/AndroidLauncher.java
@@ -1,10 +1,8 @@
 package com.asda.zombiex.android;
 
 import android.net.wifi.WifiManager;
-import android.os.Build;
 import android.os.Bundle;
 import android.text.format.Formatter;
-import android.view.View;
 
 import com.asda.zombiex.Game;
 import com.badlogic.gdx.backends.android.AndroidApplication;
@@ -15,21 +13,11 @@ public class AndroidLauncher extends AndroidApplication {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        View decorView = getWindow().getDecorView();
-        if (Build.VERSION.SDK_INT >= 19) {
-            decorView.setSystemUiVisibility(
-                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                            | View.SYSTEM_UI_FLAG_FULLSCREEN
-                            | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
-        }
-
         WifiManager wm = (WifiManager) getSystemService(WIFI_SERVICE);
         String ip = Formatter.formatIpAddress(wm.getConnectionInfo().getIpAddress()); // TODO: support only IPv4
         // TODO: send new IP when we have connect later
         AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+        config.useImmersiveMode = true;
         initialize(new Game(ip), config);
     }
 }
